4d6a2ba10008176214cce4e496afd7a71c2e69a8
Cristian Enguídanos Nebot <cristian.enguidanos.nebot@gmail.com>
2023-06-15 20:07:16 +0200

    feature: add default detail to problem details builders

--- src/libs/problem_details/mod.rs
+++ src/libs/problem_details/mod.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Default, Clone, Debug, Serialize, Deserialize)]
 pub struct ProblemDetails {
     #[serde(rename = "type")]
-    pub typee: String,
+    pub ttype: String,
     pub title: String,
     pub detail: String,
     pub status: u16,
@@ -11,43 +11,55 @@ pub struct ProblemDetails {
 
 impl ProblemDetails {
     pub fn from_400() -> Self {
+        let msg = "Bad Request";
+
         let mut problem_details = Self::default();
         problem_details
             .set_type("https://www.rfc-editor.org/rfc/rfc9110.html#name-400-bad-request")
             .set_status(400)
-            .set_title("Bad Request");
+            .set_title(msg)
+            .set_detail(msg);
         problem_details
     }
 
     pub fn from_401() -> Self {
+        let msg = "Unauthorized";
+
         let mut problem_details = Self::default();
         problem_details
             .set_type("https://www.rfc-editor.org/rfc/rfc9110.html#name-401-unauthorized")
             .set_status(401)
-            .set_title("Unauthorized");
+            .set_title(msg)
+            .set_detail(msg);
         problem_details
     }
 
     pub fn from_403() -> Self {
+        let msg = "Forbidden";
+
         let mut problem_details = Self::default();
         problem_details
             .set_type("https://www.rfc-editor.org/rfc/rfc9110.html#name-403-forbidden")
             .set_status(403)
-            .set_title("Forbidden");
+            .set_title(msg)
+            .set_detail(msg);
         problem_details
     }
 
     pub fn from_503() -> Self {
+        let msg = "Service Unavailable";
+
         let mut problem_details = Self::default();
         problem_details
             .set_type("https://www.rfc-editor.org/rfc/rfc9110.html#name-503-service-unavailable")
             .set_status(503)
-            .set_title("Service Unavailable");
+            .set_title(msg)
+            .set_detail(msg);
         problem_details
     }
 
     pub fn set_type(&mut self, value: impl ToString) -> &mut Self {
-        self.typee = value.to_string();
+        self.ttype = value.to_string();
         self
     }
 
